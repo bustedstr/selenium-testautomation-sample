b4fc845ab37a209a4d3adeff992cfb4605c214bf
bustedstr <55255448+bustedstr@users.noreply.github.com>
2025-09-15 22:55:10 +0100

    Update FirstAutomatedTest.java

--- src/test/java/tests/FirstAutomatedTest.java
+++ src/test/java/tests/FirstAutomatedTest.java
@@ -27,7 +27,7 @@ public class FirstAutomatedTest {
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+        driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options);
     }
 
     @Test
